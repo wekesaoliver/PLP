5466c3d74d7368677d511941f23fdc0018ebd6d3
wekesaoliver <oliverwekesa@gmail.com>
2024-11-09 17:46:46 +0300

    Complete challenge two

---
+++ Day_2/even_odd.py
@@ -0,0 +1,7 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(number, "is even! ⚖️")
+    else:
+        print(number, "is odd! 🎯")
+
+check_even_odd(17) # Try with different numbers!
